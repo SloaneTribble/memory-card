5b1e28e6555c584933297930109bf3903b729f7d
Sloane Tribble <SloaneTribble@gmail.com>
2022-08-14 17:27:56 -0700

    Add Header component

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,7 +1,13 @@
 import React from "react";
 
+import { Header } from "./Header";
+
 function MainContainer() {
-  return <div className="main-container"></div>;
+  return (
+    <div className="main-container">
+      <Header />
+    </div>
+  );
 }
 
 export { MainContainer };
